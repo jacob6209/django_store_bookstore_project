d5b3c57c35f6758c5b71a10e822c02711fb90d7c
Jac <ebrahimi_h2o@yahoo.com>
2023-01-12 15:36:59 +0400

    add BookListView

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-from models import book
+from .models import book
 admin.site.register(book)
--- books/models.py
+++ books/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class book(models.Model):
      title=models.CharField(max_length=200)
      author=models.CharField(max_length=200)
-     content=models.TextField
+     description=models.TextField()
      price=models.DecimalField(max_digits=5,decimal_places=2) # For Mony Use => Decimal Field
+
      def __str__(self):
           return self.title

63e29a393dd6f11f64fc5712ed3d651e27dd2505
Jac <ebrahimi_h2o@yahoo.com>
2023-01-13 00:04:25 +0400

    add BookUpdateView

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 
 from .models import book
 
-class BookListView(generic.ListView ):
+class BookListView(generic.ListView):
     model = book
     template_name = "books/book_list.html"
     context_object_name = 'books'
@@ -11,5 +11,15 @@ class BookListView(generic.ListView ):
 class BookDetailView(generic.DetailView):
     model = book
     template_name = 'books/book_detail.html'
+
+class BookCreateView(generic.CreateView):
+    model = book
+    fields = ['title','author','description','price']
+    template_name = 'books/book_create.html'
+class BookUpdateView(generic.UpdateView):
+    model = book
+    fields = ['title', 'author', 'description','price']
+    template_name = 'books/book_update.html'
+
 
 

4317679228d4cf9f32afa1ffb30b41e8bc7b7e5d
Jac <ebrahimi_h2o@yahoo.com>
2023-01-14 18:42:26 +0400

    add frontend template and static files

--- config/settings.py
+++ config/settings.py
@@ -120,17 +120,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = "accounts.CustomUser"
 
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# authentication setting
+AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 # crispy Forms setting
 CRISPY_TEMPLATE_PACK="bootstrap4"
+# Email Config
 EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
+
+#static file config
+STATIC_URL = "/static/"
+STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]

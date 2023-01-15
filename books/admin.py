821ef640442388278ad1862b7cbdb950dbea8902
Jac <ebrahimi_h2o@yahoo.com>
2023-01-15 09:41:15 +0400

    add related to book fk in comment

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 from .models import book
+from .models import Comment
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user','book','text','datetime_create',)
+
+
 admin.site.register(book)
+# admin.site.register(Comment,CommentAdmin)
+
+
+

9657f477ee08a316827cbed014aa4cdc35b951d6
Jac <ebrahimi_h2o@yahoo.com>
2025-10-27 23:27:30 +0400

    fix some bugs

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # third party
-    "crispy_forms",
+    'crispy_forms',
+    'crispy_bootstrap4',
+
 
 
     # local app
@@ -125,7 +127,6 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
